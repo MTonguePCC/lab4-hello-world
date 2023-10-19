0a25aaa0908259c993fa6ae7fd6be734f6b07377
Matthew Tongue <matthew.tongue@pcc.edu>
2023-10-18 20:43:16 -0700

    Added main method that prints a greeting.

--- src/edu/pcc/cis/conversion/HelloWorld.java
+++ src/edu/pcc/cis/conversion/HelloWorld.java
@@ -1,4 +1,15 @@
 package edu.pcc.cis.conversion;
 
-public class HelloWorld {
+/**
+ * Prints a welcoming message to the console.
+ *
+ * @author Matthew Tongue
+ * @version 2023.10.23
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
 }
